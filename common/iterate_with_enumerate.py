27330f115af8af3d4172eb61818b9606e619cdaa
Hanam824 <thehuydhbk@gmail.com>
2024-12-07 21:47:42 +0900

    fix wrong syntax

--- common/iterate_with_enumerate.py
+++ common/iterate_with_enumerate.py
@@ -5,7 +5,7 @@ raw_data = [1, 2, -1, -4, 5, 11, -0.5, 25]
 # simple
 for idx in range(len(raw_data)):
     if raw_data[idx] < 0:
-        raw_data[idx] == 0
+        raw_data[idx] = 0
 print(raw_data)
 
 # pro
